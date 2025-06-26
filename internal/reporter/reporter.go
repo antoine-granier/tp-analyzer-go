b08084a99ef4a2b8148db855b536d0c694564316
antoine-granier <78824041+antoine-granier@users.noreply.github.com>
2025-06-26 10:48:38 +0200

    add export report function

--- internal/reporter/reporter.go
+++ internal/reporter/reporter.go
@@ -1,1 +1,15 @@
 package reporter
+
+import (
+	"encoding/json"
+	"main/internal/analyzer"
+	"os"
+)
+
+func Export(results []analyzer.Result, path string) error {
+	data, err := json.MarshalIndent(results, "", "  ")
+	if err != nil {
+		return err
+	}
+	return os.WriteFile(path, data, 0644)
+}
