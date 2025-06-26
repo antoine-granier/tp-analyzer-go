07e70bc3665d65dabb30c572b0bdc907592e4501
antoine-granier <78824041+antoine-granier@users.noreply.github.com>
2025-06-26 10:44:05 +0200

    add analyzeLog function

--- internal/analyzer/analyzer.go
+++ internal/analyzer/analyzer.go
@@ -1,1 +1,35 @@
 package analyzer
+
+import (
+	"math/rand"
+	"time"
+)
+
+type Result struct {
+	LogID        string `json:"log_id"`
+	FilePath     string `json:"file_path"`
+	Status       string `json:"status"`
+	Message      string `json:"message"`
+	ErrorDetails string `json:"error_details"`
+}
+
+func AnalyzeLog(id, path string, resultChan chan<- Result) {
+	var result Result
+	result.LogID = id
+	result.FilePath = path
+
+	time.Sleep(time.Duration(rand.Intn(150)+50) * time.Millisecond)
+
+	if rand.Float32() < 0.1 {
+		parseErr := &ParseError{ID: id}
+		result.Status = "FAILED"
+		result.Message = "Erreur de parsing."
+		result.ErrorDetails = parseErr.Error()
+		resultChan <- result
+		return
+	}
+
+	result.Status = "OK"
+	result.Message = "Analyse terminée avec succès."
+	resultChan <- result
+}
