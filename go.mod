472f5a2acd3bc97dcc475c1bc51132d895307e68
antoine-granier <78824041+antoine-granier@users.noreply.github.com>
2025-06-26 10:28:29 +0200

    add log config

--- go.mod
+++ go.mod
@@ -1,3 +1,13 @@
-module github.com/antoine-granier/tp-analyzer-go
+module main
 
 go 1.24
+
+require (
+	github.com/antoine-granier/tp-analyzer-go v0.0.0-20250613144015-d2662e084fe2
+	github.com/spf13/cobra v1.9.1
+)
+
+require (
+	github.com/inconshreveable/mousetrap v1.1.0 // indirect
+	github.com/spf13/pflag v1.0.6 // indirect
+)
--- internal/config/config.go
+++ internal/config/config.go
@@ -1,1 +1,26 @@
 package config
+
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+)
+
+type LogConfig struct {
+	ID   string `json:"id"`
+	Path string `json:"path"`
+	Type string `json:"type"`
+}
+
+func LoadConfig(path string) ([]LogConfig, error) {
+	data, err := os.ReadFile(path)
+	if err != nil {
+		return nil, err
+	}
+
+	var configs []LogConfig
+	if err := json.Unmarshal(data, &configs); err != nil {
+		return nil, fmt.Errorf("Failed to unmarshal %s : %w", path, err)
+	}
+	return configs, err
+}
