1172d7a884ef43f79b9c12b726547cea59d1c2e7
Dany <dany@MacBook-Pro-de-Dany.local>
2025-06-26 11:00:07 +0200

    add command entry

--- cmd/root.go
+++ cmd/root.go
@@ -1,1 +1,14 @@
 package cmd
+
+import (
+    "github.com/spf13/cobra"
+)
+
+var rootCmd = &cobra.Command{
+    Use:   "loganalyzer",
+    Short: "Analyseur de logs",
+}
+
+func Execute() error {
+    return rootCmd.Execute()
+}
