16bb58969ed32c3b730211e6f2347f0cc2710e2c
Dany <dany@MacBook-Pro-de-Dany.local>
2025-06-26 10:32:46 +0200

    add errors hangling

--- internal/analyzer/analyzer.go
+++ internal/analyzer/analyzer.go
@@ -1,1 +1,24 @@
 package analyzer
+
+import "fmt"
+
+type FileError struct {
+    Path string
+    Err  error
+}
+
+func (e FileError) Error() string {
+    return fmt.Sprintf("Erreur fichier: %s - %v", e.Path, e.Err)
+}
+
+func (eFileError) Unwrap() error {
+    return e.Err
+}
+
+type ParseError struct {
+    ID string
+}
+
+func (e *ParseError) Error() string {
+    return fmt.Sprintf("Erreur de parsing du log: %s", e.ID)
+}
